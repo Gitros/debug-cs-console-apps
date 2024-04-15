f47fcefd6154e6d2cee617d3086ad7954fad497e
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-15 13:20:56 +0200

    code sample for a compiler-generetad exceptions

--- Program.cs
+++ Program.cs
@@ -1,17 +1,5 @@
-﻿try
-{
-    // Step 1: code execution begins
-    try
-    {
-        // Step 2: an exception occurs here
-    }
-    finally
-    {
-        // Step 4: the system executes the finally code block associated with the try statement where the exception occurred
-    }
+﻿string[] names = { "Dog", "Cat", "Fish" };
+Object[] objs = (Object[])names;
 
-}
-catch // Step 3: the system finds a catch clause that can handle the exception
-{
-    // Step 5: the system transfers control to the first line of the catch code block
-}
+Object obj = (Object)13;
+objs[2] = obj; // ArrayTypeMismatchException occurs
